1e4d4b6aac898eeb6e4ad193b95123166d72e27d
s38449 <r.bolgov@kmz.su>
2024-01-06 12:32:30 +0300

    рефакторинг

--- src/lib.c
+++ src/lib.c
@@ -15,7 +15,21 @@
     \return Контрольную сумму
     \brief Ручное вычисление контрольной суммы. Необходимо для заполнения таблицы в \ref crc8_init_value
 */
-static inline uint8_t crc8(uint8_t poly, int refin, uint8_t init);
+static inline uint8_t crc8(uint8_t poly, int refin, uint8_t init) {
+    int i = 8;
+
+    if(refin) {
+        while(i--) {
+            init = (init >> 1) ^ (poly & -(init & 1));
+        }
+    } else {
+        while(i--) {
+            init = (init << 1) ^ (poly & -((init >> 7) & 1));
+        }
+    }
+
+    return init;
+}
 /**
     \param poly Порождающий многочлен
     \param refin Начало и направление вычислений
@@ -23,7 +37,22 @@ static inline uint8_t crc8(uint8_t poly, int refin, uint8_t init);
     \return Контрольную сумму
     \brief Ручное вычисление контрольной суммы. Необходимо для заполнения таблицы в \ref crc16_init_value
 */
-static inline uint16_t crc16(uint16_t poly, int refin, uint16_t init);
+static inline uint16_t crc16(uint16_t poly, int refin, uint16_t init) {
+    int i = 8;
+
+    if(refin) {
+        while(i--) {
+            init = (init >> 1) ^ (poly & -(init & 1));
+        }
+    } else {
+        init <<= 8;
+        while(i--) {
+            init = (init << 1) ^ (poly & -((init >> 15) & 1));
+        }
+    }
+
+    return init;
+}
 /**
     \param poly Порождающий многочлен
     \param refin Начало и направление вычислений
@@ -31,7 +60,22 @@ static inline uint16_t crc16(uint16_t poly, int refin, uint16_t init);
     \return Контрольную сумму
     \brief Ручное вычисление контрольной суммы. Необходимо для заполнения таблицы в \ref crc32_init_value
 */
-static inline uint32_t crc32(uint32_t poly, int refin, uint32_t init);
+static inline uint32_t crc32(uint32_t poly, int refin, uint32_t init) {
+    int i = 8;
+
+    if(refin) {
+        while(i--) {
+            init = (init >> 1) ^ (poly & -(init & 1));
+        }
+    } else {
+        init <<= 24;
+        while(i--) {
+            init = (init << 1) ^ (poly & -((init >> 31) & 1));
+        }
+    }
+
+    return init;
+}
 /**
     \param poly Порождающий многочлен
     \param refin Начало и направление вычислений
@@ -39,7 +83,22 @@ static inline uint32_t crc32(uint32_t poly, int refin, uint32_t init);
     \return Контрольную сумму
     \brief Ручное вычисление контрольной суммы. Необходимо для заполнения таблицы в \ref crc64_init_value
 */
-static inline uint64_t crc64(uint64_t poly, int refin, uint64_t init);
+static inline uint64_t crc64(uint64_t poly, int refin, uint64_t init) {
+    int i = 8;
+
+    if(refin) {
+        while(i--) {
+            init = (init >> 1) ^ (poly & -(init & 1));
+        }
+    } else {
+        init <<= 56;
+        while(i--) {
+            init = (init << 1) ^ (poly & -((init >> 63) & 1));
+        }
+    }
+
+    return init;
+}
 /**
     \param[out] table Таблица для заполнения
     \param width Степень порождающего многочлена
@@ -47,7 +106,22 @@ static inline uint64_t crc64(uint64_t poly, int refin, uint64_t init);
     \param refin Начало и направление вычислений
     \brief Инициализация таблицы
 */
-static void crc8_table_init(uint8_t *table, int width, uint8_t poly, int refin);
+static void crc8_table_init(uint8_t *table, int width, uint8_t poly, int refin) {
+    int i = 256;
+
+    if(!table) return;
+
+    if(refin) {
+        poly = rev8(poly);
+        poly >>= 8 - width;
+    } else {
+        poly <<= 8 - width;
+    }
+
+    while(i--) {
+        table[i] = crc8(poly, refin, i);
+    }
+}
 /**
     \param[out] table Таблица для заполнения
     \param width Степень порождающего многочлена
@@ -55,7 +129,22 @@ static void crc8_table_init(uint8_t *table, int width, uint8_t poly, int refin);
     \param refin Начало и направление вычислений
     \brief Инициализация таблицы
 */
-static void crc16_table_init(uint16_t *table, int width, uint16_t poly, int refin);
+static void crc16_table_init(uint16_t *table, int width, uint16_t poly, int refin) {
+    int i = 256;
+
+    if(!table) return;
+
+    if(refin) {
+        poly = rev16(poly);
+        poly >>= 16 - width;
+    } else {
+        poly <<= 16 - width;
+    }
+
+    while(i--) {
+        table[i] = crc16(poly, refin, i);
+    }
+}
 /**
     \param[out] table Таблица для заполнения
     \param width Степень порождающего многочлена
@@ -63,7 +152,22 @@ static void crc16_table_init(uint16_t *table, int width, uint16_t poly, int refi
     \param refin Начало и направление вычислений
     \brief Инициализация таблицы
 */
-static void crc32_table_init(uint32_t *table, int width, uint32_t poly, int refin);
+static void crc32_table_init(uint32_t *table, int width, uint32_t poly, int refin) {
+    int i = 256;
+
+    if(!table) return;
+
+    if(refin) {
+        poly = rev32(poly);
+        poly >>= 32 - width;
+    } else {
+        poly <<= 32 - width;
+    }
+
+    while(i--) {
+        table[i] = crc32(poly, refin, i);
+    }
+}
 /**
     \param[out] table Таблица для заполнения
     \param width Степень порождающего многочлена
@@ -71,7 +175,22 @@ static void crc32_table_init(uint32_t *table, int width, uint32_t poly, int refi
     \param refin Начало и направление вычислений
     \brief Инициализация таблицы
 */
-static void crc64_table_init(uint64_t *table, int width, uint64_t poly, int refin);
+static void crc64_table_init(uint64_t *table, int width, uint64_t poly, int refin) {
+    int i = 256;
+
+    if(!table) return;
+
+    if(refin) {
+        poly = rev64(poly);
+        poly >>= 64 - width;
+    } else {
+        poly <<= 64 - width;
+    }
+
+    while(i--) {
+        table[i] = crc64(poly, refin, i);
+    }
+}
 /**
     \param init Стартовые данные
     \param width Степень порождающего многочлена
@@ -79,7 +198,9 @@ static void crc64_table_init(uint64_t *table, int width, uint64_t poly, int refi
     \return Значение контрольной суммы с которой будет начинаться вычисление \ref Crc8::value
     \brief Инициализация \ref Crc8::value
 */
-static inline uint8_t crc8_init_value(uint8_t init, int width, int refin);
+static inline uint8_t crc8_init_value(uint8_t init, int width, int refin) {
+    return refin ? rev8(init) >> (8 - width) : init << (8 - width);
+}
 /**
     \param init Стартовые данные
     \param width Степень порождающего многочлена
@@ -87,7 +208,9 @@ static inline uint8_t crc8_init_value(uint8_t init, int width, int refin);
     \return Значение контрольной суммы с которой будет начинаться вычисление \ref Crc16::value
     \brief Инициализация \ref Crc16::value
 */
-static inline uint16_t crc16_init_value(uint16_t init, int width, int refin);
+static inline uint16_t crc16_init_value(uint16_t init, int width, int refin) {
+    return refin ? rev16(init) >> (16 - width) : init << (16 - width);
+}
 /**
     \param init Стартовые данные
     \param width Степень порождающего многочлена
@@ -95,7 +218,9 @@ static inline uint16_t crc16_init_value(uint16_t init, int width, int refin);
     \return Значение контрольной суммы с которой будет начинаться вычисление \ref Crc32::value
     \brief Инициализация \ref Crc32::value
 */
-static inline uint32_t crc32_init_value(uint32_t init, int width, int refin);
+static inline uint32_t crc32_init_value(uint32_t init, int width, int refin) {
+    return refin ? rev32(init) >> (32 - width) : init << (32 - width);
+}
 /**
     \param init Стартовые данные
     \param width Степень порождающего многочлена
@@ -103,7 +228,9 @@ static inline uint32_t crc32_init_value(uint32_t init, int width, int refin);
     \return Значение контрольной суммы с которой будет начинаться вычисление \ref Crc64::value
     \brief Инициализация \ref Crc64::value
 */
-static inline uint64_t crc64_init_value(uint64_t init, int width, int refin);
+static inline uint64_t crc64_init_value(uint64_t init, int width, int refin) {
+    return refin ? rev64(init) >> (64 - width) : init << (64 - width);
+}
 
 void crc8_init_static(Crc8 *crc, const Crc8BasedAlgo *algo, uint8_t *table) {
     if(!crc || !algo || !table) return;
@@ -141,7 +268,7 @@ void crc8_init(Crc8 *crc, const Crc8BasedAlgo *algo) {
     uint8_t *table;
 
     if(!crc || !algo) return;
-    table = malloc(256 * sizeof(uint8_t));
+    table = malloc(256 * sizeof(*table));
     if(!table) return;
     crc8_init_static(crc, algo, table);
 }
@@ -150,7 +277,7 @@ void crc16_init(Crc16 *crc, const Crc16BasedAlgo *algo) {
     uint16_t *table;
 
     if(!crc || !algo) return;
-    table = malloc(256 * sizeof(uint16_t));
+    table = malloc(256 * sizeof(*table));
     if(!table) return;
     crc16_init_static(crc, algo, table);
 }
@@ -159,7 +286,7 @@ void crc32_init(Crc32 *crc, const Crc32BasedAlgo *algo) {
     uint32_t *table;
 
     if(!crc || !algo) return;
-    table = malloc(256 * sizeof(uint32_t));
+    table = malloc(256 * sizeof(*table));
     if(!table) return;
     crc32_init_static(crc, algo, table);
 }
@@ -168,7 +295,7 @@ void crc64_init(Crc64 *crc, const Crc64BasedAlgo *algo) {
     uint64_t *table;
 
     if(!crc || !algo) return;
-    table = malloc(256 * sizeof(uint64_t));
+    table = malloc(256 * sizeof(*table));
     if(!table) return;
     crc64_init_static(crc, algo, table);
 }
@@ -313,157 +440,6 @@ uint32_t crc32_checksum(Crc32 *crc, const void *bytes, size_t size) {
 uint64_t crc64_checksum(Crc64 *crc, const void *bytes, size_t size) {
     crc64_update(crc, bytes, size);
     return crc64_finalize(crc);
-}
-
-static inline uint8_t crc8(uint8_t poly, int refin, uint8_t init) {
-    int i = 8;
-
-    if(refin) {
-        while(i--) {
-            init = (init >> 1) ^ (poly & -(init & 1));
-        }
-    } else {
-        while(i--) {
-            init = (init << 1) ^ (poly & -((init >> 7) & 1));
-        }
-    }
-
-    return init;
-}
-
-static inline uint16_t crc16(uint16_t poly, int refin, uint16_t init) {
-    int i = 8;
-
-    if(refin) {
-        while(i--) {
-            init = (init >> 1) ^ (poly & -(init & 1));
-        }
-    } else {
-        init <<= 8;
-        while(i--) {
-            init = (init << 1) ^ (poly & -((init >> 15) & 1));
-        }
-    }
-
-    return init;
-}
-
-static inline uint32_t crc32(uint32_t poly, int refin, uint32_t init) {
-    int i = 8;
-
-    if(refin) {
-        while(i--) {
-            init = (init >> 1) ^ (poly & -(init & 1));
-        }
-    } else {
-        init <<= 24;
-        while(i--) {
-            init = (init << 1) ^ (poly & -((init >> 31) & 1));
-        }
-    }
-
-    return init;
-}
-
-static inline uint64_t crc64(uint64_t poly, int refin, uint64_t init) {
-    int i = 8;
-
-    if(refin) {
-        while(i--) {
-            init = (init >> 1) ^ (poly & -(init & 1));
-        }
-    } else {
-        init <<= 56;
-        while(i--) {
-            init = (init << 1) ^ (poly & -((init >> 63) & 1));
-        }
-    }
-
-    return init;
-}
-
-static void crc8_table_init(uint8_t *table, int width, uint8_t poly, int refin) {
-    int i = 256;
-
-    if(!table) return;
-
-    if(refin) {
-        poly = rev8(poly);
-        poly >>= 8 - width;
-    } else {
-        poly <<= 8 - width;
-    }
-
-    while(i--) {
-        table[i] = crc8(poly, refin, i);
-    }
-}
-
-static void crc16_table_init(uint16_t *table, int width, uint16_t poly, int refin) {
-    int i = 256;
-
-    if(!table) return;
-
-    if(refin) {
-        poly = rev16(poly);
-        poly >>= 16 - width;
-    } else {
-        poly <<= 16 - width;
-    }
-
-    while(i--) {
-        table[i] = crc16(poly, refin, i);
-    }
-}
-
-static void crc32_table_init(uint32_t *table, int width, uint32_t poly, int refin) {
-    int i = 256;
-
-    if(!table) return;
-
-    if(refin) {
-        poly = rev32(poly);
-        poly >>= 32 - width;
-    } else {
-        poly <<= 32 - width;
-    }
-
-    while(i--) {
-        table[i] = crc32(poly, refin, i);
-    }
-}
-
-static void crc64_table_init(uint64_t *table, int width, uint64_t poly, int refin) {
-    int i = 256;
-
-    if(!table) return;
-
-    if(refin) {
-        poly = rev64(poly);
-        poly >>= 64 - width;
-    } else {
-        poly <<= 64 - width;
-    }
-
-    while(i--) {
-        table[i] = crc64(poly, refin, i);
-    }
-}
-
-static inline uint8_t crc8_init_value(uint8_t init, int width, int refin) {
-    return refin ? rev8(init) >> (8 - width) : init << (8 - width);
-}
-
-static inline uint16_t crc16_init_value(uint16_t init, int width, int refin) {
-    return refin ? rev16(init) >> (16 - width) : init << (16 - width);
-}
-
-static inline uint32_t crc32_init_value(uint32_t init, int width, int refin) {
-    return refin ? rev32(init) >> (32 - width) : init << (32 - width);
-}
-
-static inline uint64_t crc64_init_value(uint64_t init, int width, int refin) {
-    return refin ? rev64(init) >> (64 - width) : init << (64 - width);
 }
 
 #ifdef _INT128_DEFINED
