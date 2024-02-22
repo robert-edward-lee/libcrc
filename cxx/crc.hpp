e435359d7941f845e8005144c071db6be7d3b8f0
s38449 <r.bolgov@kmz.su>
2024-02-22 14:00:42 +0300

    добавлена поддержка `__builtin_bitreverse`

--- cxx/crc.hpp
+++ cxx/crc.hpp
@@ -10,31 +10,53 @@
 
 namespace crc {
 
+#ifdef __has_builtin
+#if __has_builtin(__builtin_convertvector) && defined(__clang__)
+#define __has_builtin_bitreverse
+#endif
+#endif
+
 static inline uint8_t rev(uint8_t x) {
+#ifdef __has_builtin_bitreverse
+    return __builtin_bitreverse8(x);
+#else
     x = ((x & 0x55) << 1) | ((x & 0xAA) >> 1);
     x = ((x & 0x33) << 2) | ((x & 0xCC) >> 2);
     return x << 4 | x >> 4;
+#endif
 }
 static inline uint16_t rev(uint16_t x) {
+#ifdef __has_builtin_bitreverse
+    return __builtin_bitreverse16(x);
+#else
     x = ((x & 0x5555) << 1) | ((x & 0xAAAA) >> 1);
     x = ((x & 0x3333) << 2) | ((x & 0xCCCC) >> 2);
     x = ((x & 0x0F0F) << 4) | ((x & 0xF0F0) >> 4);
     return x << 8 | x >> 8;
+#endif
 }
 static inline uint32_t rev(uint32_t x) {
+#ifdef __has_builtin_bitreverse
+    return __builtin_bitreverse32(x);
+#else
     x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1);
     x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2);
     x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4);
     x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8);
     return x << 16 | x >> 16;
+#endif
 }
 static inline uint64_t rev(uint64_t x) {
+#ifdef __has_builtin_bitreverse
+    return __builtin_bitreverse64(x);
+#else
     x = ((x & 0x5555555555555555) << 1) | ((x & 0xAAAAAAAAAAAAAAAA) >> 1);
     x = ((x & 0x3333333333333333) << 2) | ((x & 0xCCCCCCCCCCCCCCCC) >> 2);
     x = ((x & 0x0F0F0F0F0F0F0F0F) << 4) | ((x & 0xF0F0F0F0F0F0F0F0) >> 4);
     x = ((x & 0x00FF00FF00FF00FF) << 8) | ((x & 0xFF00FF00FF00FF00) >> 8);
     x = ((x & 0x0000FFFF0000FFFF) << 16) | ((x & 0xFFFF0000FFFF0000) >> 16);
     return x << 32 | x >> 32;
+#endif
 }
 
 #ifdef __SIZEOF_INT128__
