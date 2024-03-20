ba9b6dabaec837abc24197cd21939ecd82788842
s38449 <r.bolgov@kmz.su>
2024-03-20 15:31:24 +0300

    исправлена ошибка `using` деклараций

--- include/crcxx/detail/traits.hxx
+++ include/crcxx/detail/traits.hxx
@@ -7,20 +7,10 @@
 namespace crc { namespace detail {
 #if CRCXX_STDCXX_VERSION_CHECK(201103)
 #include <type_traits>
-template<bool Cond, typename T = void> using enable_if = std::enable_if<Cond, T>;
-template<typename> struct is_byte: std::false_type {};
-template<> struct is_byte<int8_t>: std::true_type {};
-template<> struct is_byte<uint8_t>: std::true_type {};
-
-template<typename T> struct is_valid_crc_type_helper: std::false_type {};
-template<> struct is_valid_crc_type_helper<uint8_t>: std::true_type {};
-template<> struct is_valid_crc_type_helper<uint16_t>: std::true_type {};
-template<> struct is_valid_crc_type_helper<uint32_t>: std::true_type {};
-template<> struct is_valid_crc_type_helper<uint64_t>: std::true_type {};
-#if CRCXX_HAS_128BIT_ALGO
-template<> struct is_valid_crc_type_helper<uint128_t>: std::true_type {};
-#endif
-template<typename T> struct is_valid_crc_type: is_valid_crc_type_helper<typename std::remove_cv<T>::type> {};
+using std::true_type;
+using std::false_type;
+using std::enable_if;
+using std::remove_cv;
 #else
 template<typename T, T v> struct integral_constant {
     static const T value = v;
@@ -32,16 +22,11 @@ template<typename T, T v> struct integral_constant {
 };
 typedef integral_constant<bool, true> true_type;
 typedef integral_constant<bool, false> false_type;
+
 template<bool, typename T = void> struct enable_if {};
 template<typename T> struct enable_if<true, T> {
     typedef T type;
 };
-template<typename T, typename U> struct is_same: false_type {};
-template<typename T> struct is_same<T, T>: true_type {};
-
-template<typename> struct is_byte: false_type {};
-template<> struct is_byte<int8_t>: true_type {};
-template<> struct is_byte<uint8_t>: true_type {};
 
 template<typename T> struct remove_cv {
     typedef T type;
@@ -55,6 +40,11 @@ template<typename T> struct remove_cv<volatile T> {
 template<typename T> struct remove_cv<const volatile T> {
     typedef T type;
 };
+#endif
+
+template<typename> struct is_byte: false_type {};
+template<> struct is_byte<int8_t>: true_type {};
+template<> struct is_byte<uint8_t>: true_type {};
 
 template<typename T> struct is_valid_crc_type_helper: false_type {};
 template<> struct is_valid_crc_type_helper<uint8_t>: true_type {};
@@ -65,7 +55,6 @@ template<> struct is_valid_crc_type_helper<uint64_t>: true_type {};
 template<> struct is_valid_crc_type_helper<uint128_t>: true_type {};
 #endif
 template<typename T> struct is_valid_crc_type: is_valid_crc_type_helper<typename remove_cv<T>::type> {};
-#endif
 }} // namespace crc::detail
 
 #endif // HXX_CXX_INTERNAL_TRAITS
--- include/crcxx/detail/types.hxx
+++ include/crcxx/detail/types.hxx
@@ -6,6 +6,10 @@
 namespace crc {
 #if CRCXX_STDCXX_VERSION_CHECK(201103)
 #include <cstdint>
+using std::uint8_t;
+using std::uint16_t;
+using std::uint32_t;
+using std::uint64_t;
 #else
 #include <stdint.h>
 #endif
@@ -13,7 +17,6 @@ namespace crc {
 #if CRCXX_HAS_128BIT_ALGO
 typedef __uint128_t uint128_t;
 #endif
-
 } // namespace crc
 
 #endif // HXX_CRCXX_INTERNAL_TYPES
